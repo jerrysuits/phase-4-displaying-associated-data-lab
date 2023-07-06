021ddf6da28e04051ed64641477cd12dc9e85fa0
Gerald <adudagerald@gmail.com>
2023-07-06 12:31:45 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+     def index
+          items = Item.includes(:user).all 
+          render json: items, include: :user
+     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+     def show 
+          users = User.includes(:items).find_by(id: params[:id])
+          render json: users, include: :items
+     end
 end
